33f5bee20576bffb1ba0b455035adbcf17f963c1
GillesGroulard <gillesgroulard2@gmail.com>
2024-12-11 10:52:27 +0100

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://oqdkwfddbxexdthfavig.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9xZGt3ZmRkYnhleGR0aGZhdmlnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA3MTMyMzksImV4cCI6MjA0NjI4OTIzOX0.6SJHbdRXq1lq2a3SJC-0HSsx9IN9IQerO0TxgIdF42Q";
 
 if (!supabaseUrl || !supabaseKey) {
   throw new Error('Missing Supabase configuration. Please check your environment variables.');
